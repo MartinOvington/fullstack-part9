d797d88f61fcf4c64500f1fecf11027eea941d70
Martin Ovington <MartinOvington@msn.com>
2022-05-12 09:28:24 +0100

    ex9.5 added endpoint for BMI calculator queries

--- exercise/index.ts
+++ exercise/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import calculateBmi from './calculateBmi';
 
 const app = express();
 
@@ -6,7 +7,27 @@ app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
 });
 
-const PORT = 3003;
+app.get('/bmi', (req, res) => {
+  const query = req.query;
+  if (query.height && query.weight) {
+    const height = Number(query.height);
+    const weight = Number(query.weight);
+    if (!isNaN(height) && !isNaN(weight)) {
+      const result = calculateBmi(height, weight);
+      res.send({
+        weight: weight,
+        height: height,
+        bmi: result,
+      });
+    } else {
+      res.send({ error: 'malformed parameters' });
+    }
+  } else {
+    res.send({ error: 'malformed parameters' });
+  }
+});
+
+const PORT = 3002;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
